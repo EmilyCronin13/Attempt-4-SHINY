a2334b83eb501dcdf35cc389bcc34515a58274f5
Your Name <E.cronin15@universityofgalway.ie>
2024-12-22 21:08:51 +0000

    Fixed spacing

--- app.R
+++ app.R
@@ -7,6 +7,8 @@ library(plotly)
 library(DT)
 library(ggmosaic)
 
+
+
 # The darkly theme along with filter bars is obtained from garrett gmann who works for posit cloud : https://github.com/garrettgman/shiny-styling-demo/blob/main/finished-app/app.R
 ggplot2::theme_set(ggplot2::theme_minimal())
 thematic_shiny()
@@ -17,6 +19,8 @@ ui <- navbarPage(
   theme = bs_theme(bootswatch = "darkly",
                    base_font = font_google("Lato"),
                    heading_font = font_google("Raleway")),
+  
+  
   #Adding multiple panels to the shiny page : https://www.youtube.com/watch?v=V8ne6_F7lIM
   
   #Introduction Tab to the DIG trial
@@ -35,73 +39,64 @@ padding: 50px;}
                                                       
 #Next we are adding our giphs and images to our app method is curtesy of the posit tutorial for images : https://shiny.posit.co/r/articles/build/images/         
 #All giphs included are from GIPHY and are not coprighted: https://www.youtube.com/watch?v=Ka2pWqXS1WA
-
-
+                                                      
+                                                      
 div(class = "about-page",
-    div( class = "about-header",img(src = "https://media.giphy.com/media/ToMjGpAibV4AwKryEXS/giphy.gif"),h1("Introduction to the Digitalis Investigation Group (DIG) Trial"),img(src = "https://media.giphy.com/media/l3YSj6Oirgkb18AkE/giphy.gif") ),
-    div( class = "intro-box", #Add the text to the box we generated - intro to the dig study. The summary text is from the actual DIG study : https://biolincc.nhlbi.nih.gov/studies/dig/
-         p("The Digitalis Investigation Group (DIG) trial was the first large simple trial conducted by the National Heart, Lung, and Blood Institute in conjunction with the Department of Veterans Affair."),
-         p("The Digitalis Investigation Group (DIG) study was a prospective, randomized clinical trial involving 7,788 patients with HF randomized to digoxin or placebo and followed for an average of 37 months."),
-         p("Interactions between age and the following clinical outcomes were examined: total mortality, all-cause hospitalizations, HF hospitalizations, the composite of HF death or HF hospitalizations, hospitalization for suspected digoxin toxicity and withdrawal from therapy because of side effects."),
-         p("A total of 302 centers in the United States and Canada enrolled 7,788 patients between February 1991 and September 1993.")),
-    
-    div(class = "image-container",img(src = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExazBtamw1NWtjeHVoa2Z1ejJybGxyMnQ2czhjejVrNjRxcXYyZHR0NyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/3o85xm0pDVY4EkKdFe/giphy.gif",
-                                      class = "circle-image"))))), #This is the gif but in a circle form
-
-
+div( class = "about-header",img(src = "https://media.giphy.com/media/ToMjGpAibV4AwKryEXS/giphy.gif"),h1("Introduction to the Digitalis Investigation Group (DIG) Trial"),img(src = "https://media.giphy.com/media/l3YSj6Oirgkb18AkE/giphy.gif") ),
+div( class = "intro-box", #Add the text to the box we generated - intro to the dig study. The summary text is from the actual DIG study : https://biolincc.nhlbi.nih.gov/studies/dig/
+                                                               
+p("The Digitalis Investigation Group (DIG) trial was the first large simple trial conducted by the National Heart, Lung, and Blood Institute in conjunction with the Department of Veterans Affair."),
+p("The Digitalis Investigation Group (DIG) study was a prospective, randomized clinical trial involving 7,788 patients with HF randomized to digoxin or placebo and followed for an average of 37 months."),
+p("Interactions between age and the following clinical outcomes were examined: total mortality, all-cause hospitalizations, HF hospitalizations, the composite of HF death or HF hospitalizations, hospitalization for suspected digoxin toxicity and withdrawal from therapy because of side effects."),
+p("A total of 302 centers in the United States and Canada enrolled 7,788 patients between February 1991 and September 1993.")),
+                                                          
+div(class = "image-container",img(src = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExazBtamw1NWtjeHVoa2Z1ejJybGxyMnQ2czhjejVrNjRxcXYyZHR0NyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/3o85xm0pDVY4EkKdFe/giphy.gif",
+class = "circle-image"))))), #This is the gif but in a circle form
+  
 #The next tab is the visualization tab which includes the filter slider from garrett gmans tutorial
-
 tabPanel("Data Exploration: Visualisation of DIG Trial outcomes",
-         fluidPage( div(class = "visualization-info",h3("Visual Data Exploration: Results of DIG Trial"),
-                        p("This section displays various visualizations based on the Digitalis Investigation Group (DIG) trial data. You can explore the relationship between different clinical variables, treatment groups, and patient characteristics through scatter plots, mosaic plots, and bar charts. Use the controls to filter data by age, gender, and other factors.")),
-                    sidebarLayout( sidebarPanel(sliderInput("age_range", "Age Range", min = 30, max = 100, value = c(30, 100)),
-                                                selectizeInput("gender", "Gender", choices = c("All", "Male", "Female"), selected = "All"),
-                                                selectizeInput("variable", "Variable",choices = c("BMI", "KLEVEL", "CREAT", "DIABP", "SYSBP", "HOSPDAYS"), selected = "BMI"),
-                                                
-                                                #Download button so user can get the filtered Data : https://shiny.posit.co/r/reference/shiny/1.3.1/downloadbutton
-                                                downloadButton("download_data", "Download DIG Trial Data"),
-                                                
-                                                #Add a logo: The DIG group logo : code from https://forum.posit.co/t/how-to-add-a-my-company-logo-in-r-shiny-material/35256
-                                                
-                                                tags$img(src = "https://www.digitalis-cdc.com/digitalisshowcase/ui/img/digitalis-flat-1200x800.png",
-                                                         style = "width: 100%; height: auto; margin-top: 20px;")),
-                                   #Main sub panel for the visualizations
-                                   mainPanel(tabsetPanel(
-                                     tabPanel("Interactive Scatter Plot", plotlyOutput("scatter_plot")),
-                                     tabPanel("Mosaic Plot", plotlyOutput("mosaic_plot")),
-                                     tabPanel("Static Bar Charts",fluidRow(column(6, plotlyOutput("mortality_plot")),column(6, plotlyOutput("cvd_mortality_plot")) )),
-                                     tabPanel("Static Survival Plot", #Put in small description about survival plot as it gives a broad overview of studyz
-                                              fluidPage(div(
-                                                class = "static-survival-plot",
-                                                h3("Static Survival Plot"),
-                                                p("This plot shows the risk of mortality over time by treatment group and the presence or absence of cardiovascular disease (CVD). It helps to visualize how CVD affects the mortality risk in patients under different treatments."),
-                                                img(src = "survival.png", style = "width: 100%; height: auto; margin-top: 20px;"))))))))),
-
-#Summary panel page
-tabPanel("Summary of Findings",
-         fluidPage(tags$head(tags$style(HTML(".summary-table-section {background-color: #2C3E50;padding: 20px;color: white;border-radius: 10px;margin-bottom: 20px;}
+fluidPage( div(class = "visualization-info",h3("Visual Data Exploration: Results of DIG Trial"),
+p("This section displays various visualizations based on the Digitalis Investigation Group (DIG) trial data. You can explore the relationship between different clinical variables, treatment groups, and patient characteristics through scatter plots, mosaic plots, and bar charts. Use the controls to filter data by age, gender, and other factors.")),
+sidebarLayout( sidebarPanel(sliderInput("age_range", "Age Range", min = 30, max = 100, value = c(30, 100)),
+selectizeInput("gender", "Gender", choices = c("All", "Male", "Female"), selected = "All"),
+selectizeInput("variable", "Variable",choices = c("BMI", "KLEVEL", "CREAT", "DIABP", "SYSBP", "HOSPDAYS"), selected = "BMI"),
+                                                  
+#Download button so user can get the filtered Data : https://shiny.posit.co/r/reference/shiny/1.3.1/downloadbutton
+downloadButton("download_data", "Download DIG Trial Data"),
+                                                  
+ #Add a logo: The DIG group logo : code from https://forum.posit.co/t/how-to-add-a-my-company-logo-in-r-shiny-material/35256
+                                                  
+tags$img(src = "https://www.digitalis-cdc.com/digitalisshowcase/ui/img/digitalis-flat-1200x800.png",
+style = "width: 100%; height: auto; margin-top: 20px;")),
+                                     
+#Main sub panel for the visualizations
+                                     mainPanel(tabsetPanel(
+                                       tabPanel("Interactive Scatter Plot", plotlyOutput("scatter_plot")),
+                                       tabPanel("Mosaic Plot", plotlyOutput("mosaic_plot")),
+                                       tabPanel("Static Bar Charts",fluidRow(column(6, plotlyOutput("mortality_plot")),column(6, plotlyOutput("cvd_mortality_plot")) )),
+                                       tabPanel("Static Survival Plot", #Put in small description about survival plot as it gives a broad overview of studyz
+                                                fluidPage(div(
+                                                  class = "static-survival-plot",
+                                                  h3("Static Survival Plot"),
+                                                  p("This plot shows the risk of mortality over time by treatment group and the presence or absence of cardiovascular disease (CVD). It helps to visualize how CVD affects the mortality risk in patients under different treatments."),
+                                                  img(src = "survival.png", style = "width: 100%; height: auto; margin-top: 20px;"))))))))),
+  
+  #Summary panel page
+  tabPanel("Summary of Findings",
+           fluidPage(tags$head(tags$style(HTML(".summary-table-section {background-color: #2C3E50;padding: 20px;color: white;border-radius: 10px;margin-bottom: 20px;}
 .summary-title {text-decoration: underline;font-size: 1.8em;}
 .summary-table-container {background-color: white;border-radius: 10px;padding: 10px;}
 .info-box {background-color: #2C3E50;color: white;border: 1px solid #ddd;border-radius: 10px;padding: 15px;margin-bottom: 20px;display: flex;align-items: center;gap: 10px;}
 .info-icon {font-size: 20px;color: #3498db;}"))),
-                   #Making the table interactive so you can search : Using datatables https://shiny.posit.co/r/articles/build/datatables/
-                   filtered_data <- reactive({
-                     data <- dig.df %>%
-                       filter(AGE >= input$age_range[1] & AGE <= input$age_range[2])
-                     if (input$gender != "All") {
-                       data <- data %>% filter(SEX == input$gender)}
-                     if (nrow(data) == 0) {
-                       showNotification("No data available for selected filters.", type = "warning")} # Adding a warning if someone tries to search for something else
-                     data})
-                   
-                   div(class = "summary-table-section",
-                       h2("Summary of Findings of DIG Trial", class = "summary-title", style = "text-align: center;")),
-                   div(class = "info-box",
-                       tags$span(class = "info-icon", "ℹ️"),
-                       p("This is the summary data collected during this study for both Digoxin and Placebo groups. Increasing age is associated with progressively worse clinical outcomes in patients with HF. However, the beneficial effects of digoxin in reducing all-cause admissions, HF admissions, and HF death or hospitalization are independent of age. Thus, digoxin remains a useful agent to the adjunctive treatment of HF due to impaired left ventricular systolic function in patients of all ages.")),
-                   div(
-                     class = "summary-table-container",
-                     DTOutput("summary_table"))))) #Closing all brackets
+                     
+div(class = "summary-table-section",
+h2("Summary of Findings of DIG Trial", class = "summary-title", style = "text-align: center;")),
+div(class = "info-box",
+tags$span(class = "info-icon", "ℹ️"),
+p("This is the summary data collected during this study for both Digoxin and Placebo groups. Increasing age is associated with progressively worse clinical outcomes in patients with HF. However, the beneficial effects of digoxin in reducing all-cause admissions, HF admissions, and HF death or hospitalization are independent of age. Thus, digoxin remains a useful agent to the adjunctive treatment of HF due to impaired left ventricular systolic function in patients of all ages.")),
+                     div(
+                       class = "summary-table-container",
+                       DTOutput("summary_table"))))) #Closing all brackets
 
 # Now defining our server
 server <- function(input, output, session) {
@@ -121,6 +116,7 @@ server <- function(input, output, session) {
     if (nrow(data) == 0) {
       showNotification("No data available for selected filters.", type = "warning")} # Adding a warning if someone tries to search for something else
     data})
+  
   #Output summary to user
   output$summary_table <- renderDT({
     datatable(
@@ -130,6 +126,7 @@ server <- function(input, output, session) {
         pageLength = 10,
         autoWidth = TRUE,
         dom = "Bfrtip"))})
+  
   #Visualizing our graphs pink for female and blue for male
   #Scatterplot from assignment 4
   output$scatter_plot <- renderPlotly({
@@ -189,6 +186,7 @@ server <- function(input, output, session) {
 
 # Run the final app
 shinyApp(ui, server)
+
 
 
 
